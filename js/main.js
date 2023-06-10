e2c25faeb3ff9b04446619bfd1df03e6cdba9ee3
mmtv <mmtv.news.tw@gmail.com>
2023-06-10 13:03:43 +0800

    add noindex

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
     fetch("templates/nav.html")
     .then(response => response.text())
     .then(data => {
-        document.getElementById("nav").innerHTML = data;
+        document.getElementById("nav").innerHTML = data.getElementById("nav-content").innerHTML;
     })
     .catch(error => {
         console.log(error);
