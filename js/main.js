50ee3d43058a226d4c49717b640811b8b69d0774
mmtv <mmtv.news.tw@gmail.com>
2023-06-10 13:52:28 +0800

    add footer

--- js/main.js
+++ js/main.js
@@ -10,4 +10,12 @@ $(document).ready(function () {
     .catch(error => {
         console.log(error);
     });
+    fetch("templates/footer.html")
+    .then(response => response.text())
+    .then(data => {
+        const parser = new DOMParser();
+        const htmlDoc = parser.parseFromString(data, 'text/html');
+        const footerBody = htmlDoc.querySelector('body');
+        document.getElementById("footer").innerHTML = footerBody.innerHTML;
+    })
 });
